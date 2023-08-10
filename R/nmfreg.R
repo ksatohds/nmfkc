dcddebdeb5934278bf497fad55085e7cae68899e
kenichi-satoh-biwako <kenichi-satoh@biwako.shiga-u.ac.jp>
2023-08-11 07:44:21 +0900

    08230811 7:44

---
+++ R/nmfreg.R
@@ -0,0 +1,135 @@
+#' @title Creating kernel matrix A from covariate matrix U
+#' @description \code{create.kernel} create kernel matrix A from covariate matrix U
+#' @param U covariate matrix U: KxN, each row should be normalized
+#' @param beta parameter of kernel matrix A of which element is defined by exp(-beta*|u_i-u_j|^2)
+#' @return kernel matrix A: NxN
+#' @export
+#' @examples
+#' # A <- create.kernel(U,beta=10)
+
+create.kernel <- function(U,beta){
+  kernel <- function(n){
+    un <- t(rep(1,ncol(U)) %o% U[,n])
+    exp(-beta*colSums((U-un)^2))}
+  A <- NULL
+  for(n in 1:ncol(U)) A <- rbind(A,kernel(n))
+  return(A)
+}
+
+#' @title Optimizing X and C on NMF:Y~XCA where Y and A are given
+#' @description \code{nmfreg} optimize basis matrix X whose column sum is 1 and coefficient matrix C
+#' @param Y observation matrix Y:PxN
+#' @param A kernel matrix A:NxN
+#' @param Q rank of basis matrix X:P*Q
+#' @param gamma penalty parameter for C:QxN, where objective function:tr(Y-YHAT)'(Y-YHAT)+gamma*trC'C
+#' @param iter number of iterations
+#' @return X:NxQ, C:QxN, B=XC, YHAT=XB
+#' @return err:last objective function, errs:objective function at each iteration
+#' @return r.squared: squared correlation, i.e., coefficient of determination R^2 between Y and YHAT
+#' @export
+#' @examples
+#' # library(fda)
+#' # data(CanadianWeather)
+#' # d <- CanadianWeather$dailyAv[,,1]
+#' # Y <- d-min(d)
+#' # A <- diag(ncol(Y))
+#' # result <- nmfreg(Y,A,Q=2)
+#' # result$r.squared
+#' #  n <- 1
+#' # plot(Y[,n])
+#' # lines(result$YHAT[,n])
+
+nmfreg <- function(Y,A,Q=2,gamma=0,iter=500){
+  set.seed(123)
+  res <- stats::kmeans(t(Y),centers=Q)
+  X <- t(res$centers)
+  X <- t(t(X)/colSums(X))
+  C <- matrix(stats::rnorm(ncol(X)*nrow(A),mean=2,sd=0.3),
+               nrow=ncol(X),ncol=nrow(A))
+  B <- C %*% A
+  YHAT <- X %*% B
+  errs <- 0*(1:iter)
+  for(ii in 1:iter){
+    X <- X * ((Y %*% t(B)) / (YHAT %*% t(B)))
+    X <- t(t(X)/colSums(X))
+    C <- C*((t(X)%*%Y%*%t(A))/(t(X)%*%YHAT%*%t(A)+gamma*C))
+    B <- C %*% A
+    YHAT <- X %*% B
+    errs[ii] <- sum((Y-YHAT)^2)+gamma*sum(C^2)
+  }
+  err <- sum((Y-YHAT)^2)+gamma*sum(C^2)
+  r2 <- stats::cor(as.vector(YHAT),as.vector(Y))^2
+  return(list(X=X,C=C,B=B,YHAT=YHAT,err=err,errs=errs,r.squared=r2))
+}
+
+
+#' @title Checking if kernel matrix is identity matrix
+#' @description \code{is.identity.matrix} check if kernel matrix A is identity matrix
+#' @param A kernel matrix A:NxN
+#' @return logical value
+#' @export
+#' @examples
+#' # if(is.identity.matrix(A)) print("A is identity matrix") else print("A is not identity matrix")
+
+is.identity.matrix <- function(A){
+  result <- FALSE
+  if(min(A)==0&max(A)==1){
+    result <- TRUE
+    for(i in 1:nrow(A))
+      for(j in 1:ncol(A))
+        if(i==j){
+          if(A[i,j]!=1) result <- FALSE
+        }else{
+          if(A[i,j]!=0) result <- FALSE
+        }
+  }
+  return(result)
+}
+
+
+#' @title Performing k-fold cross validation
+#' @description \code{nmfreg.cv} apply cv method for k-pertitioned columns of Y:PxN
+#' @param Y observation matrix Y:PxN
+#' @param A kernel matrix A:NxN
+#' @param Q rank of basis matrix X:P*Q
+#' @param gamma penalty parameter for C:QxN, where objective function:tr(Y-YHAT)'(Y-YHAT)+gamma*trC'C
+#' @param iter number of iterations
+#' @param div number of partition, usually referred to as "k"
+#' @param seed random seed which decides the partition at random
+#' @return X:NxQ, C:QxN, B=XC, YHAT=XB
+#' @return err:last objective function, errs:objective function at each iteration
+#' @return group: partition index between 1 and div for each column of Y
+#' @export
+#' @examples
+#' # result.cv <- nmfreg.cv(Y,A)
+
+nmfreg.cv <- function(Y,A,Q,gamma=0,iter=500,div=5,seed=123){
+  set.seed(seed)
+  index <- sample(1:div,ncol(Y),replace=T)
+  err <- 0
+  errs <- 0*(1:div)
+  for(j in 1:div){
+    Y_j <- Y[,index!=j]
+    Yj <- Y[,index==j]
+    A_j <- A[index!=j,index!=j]
+    res <- nmfreg(Y_j,A_j,Q,gamma,iter)
+    X_j <- res$X
+    C_j <- res$C
+    if(is.identity.matrix(A)){
+      # Aが単位行列の場合，つまり，説明変数なしの場合に
+      # 観測行列に対応する係数行列をX_j固定で求める
+      C_j <- matrix(stats::rnorm(ncol(X_j)*ncol(Yj),mean=2,sd=0.3),
+                    nrow=ncol(X_j),ncol=ncol(Yj))
+      for(l in 1:iter){
+        YHATj <- X_j %*% C_j
+        C_j <- C_j*((t(X_j)%*%Yj)/(t(X_j)%*%YHATj+gamma*C_j))
+      }
+      YHATj <- X_j %*% C_j
+    }else{
+      YHATj <- X_j %*% C_j %*% A[index!=j,index==j] # Aのサイズに注意！
+    }
+    errs[j] <- sum((Yj-YHATj)^2)+gamma*sum(C_j^2)
+  }
+  err <- sum(errs)
+  return(list(errs=errs,err=err,group=index))
+}
--- README.md
+++ README.md
@@ -20,7 +20,36 @@ devtools::install_github("ksatohds/nmfreg")
 This is a basic example which shows you how to solve a common problem:
 
 ``` r
+library(fda)
+data(CanadianWeather)
+d <- CanadianWeather$dailyAv[,,1]
+Y <- d-min(d)
+
+# without covariate
+A <- diag(ncol(Y))
 library(nmfreg)
-## basic example code
+result <- nmfreg(Y,A,Q=2)
+result$r.squared
+n <- 1
+plot(Y[,n])
+lines(result$YHAT[,n])
+
+# with covariates
+u0 <- t(CanadianWeather$coordinates[,2:1])
+u0[1,] = -u0[1,]
+umin <- apply(u0,1,min)
+umax <- apply(u0,1,max)
+U <- (u0-umin)/(umax-umin) # normalization
+
+myseed  <- nmfreg.cv.seed(n=ncol(Y),div=10)
+betas <- c(0.1,0.2,0.5,1,2,5,10)
+errs <- 0*betas
+for(i in 1:length(betas)){
+  print(i)
+  A <- create.kernel(U,beta=betas[i])
+  result <- nmfreg.cv(Y,A,Q=2,div=10,seed=myseed)
+  errs[i] <- result$err
+}
+plot(betas,errs,type="o",log="x")
 ```
 
