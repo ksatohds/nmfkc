0bc5619c94923c1d4f75ecca22aa7e97bca5fe1b
kenichi-satoh-biwako <kenichi-satoh@biwako.shiga-u.ac.jp>
2023-08-12 18:17:02 +0900

    20230812 1816

--- R/nmfreg.R
+++ R/nmfreg.R
@@ -16,10 +16,16 @@ create.kernel <- function(U,beta){
   return(A)
 }
 
-#' @title Optimizing X and C on NMF:Y~XCA where Y and A are given
+#' @title The goal of nmfreg is to perform NMF regression model described by Y~XCA
+#'  where observation matrix Y:PxN,
+#'  kernel matrix A:NxN with parameter beta,
+#'  rank of basis matrix X:PxQ whose column sum is 1
+#'  and coefficient matrix C:QxN,
+#'  Y and A are known, and X and C are unknown.
+#'  Optimizing X and C on NMF:Y~XCA where Y and A are given
 #' @description \code{nmfreg} optimize basis matrix X whose column sum is 1 and coefficient matrix C
 #' @param Y observation matrix Y:PxN
-#' @param A kernel matrix A:NxN
+#' @param A kernel matrix A:NxN. Set A <- diag(ncol(Y)) without covariate.
 #' @param Q rank of basis matrix X:PxQ
 #' @param gamma penalty parameter for C:QxN where objective function:tr(Y-YHAT)'(Y-YHAT)+gamma*trC'C for method="EU" and sum(-Y*log(YHAT)+YHAT)+gamma*sum(C^2) for method="KL"
 #' @param epsilon positive convergence tolerance
@@ -95,7 +101,7 @@ nmfreg <- function(Y,A,Q=2,gamma=0,epsilon=1e-4,maxit=5000,method="EU"){
 #' @title Performing k-fold cross validation
 #' @description \code{nmfreg.cv} apply cv method for k-pertitioned columns of Y:PxN
 #' @param Y observation matrix Y:PxN
-#' @param A kernel matrix A:NxN
+#' @param A kernel matrix A:NxN. Set A <- diag(ncol(Y)) without covariate.
 #' @param Q rank of basis matrix X:P*Q
 #' @param gamma penalty parameter for C:QxN, where objective function:tr(Y-YHAT)'(Y-YHAT)+gamma*trC'C
 #' @param epsilon positive convergence tolerance
