18c7bfe48c8e141a1a0b14deaa68dde0e0220648
ksatohds <kenichi-satoh@biwako.shiga-u.ac.jp>
2024-06-23 05:32:40 +0900

    20240623 0533

--- R/nmfkc.R
+++ R/nmfkc.R
@@ -1,5 +1,5 @@
 .onAttach <- function(...) {
-  packageStartupMessage("Last update on 22nd Jun 2024")
+  packageStartupMessage("Last update on 23rd Jun 2024")
   packageStartupMessage("https://github.com/ksatohds/nmfkc")
 }
 
